1709890834757d434e7a293362ddea668f3e316f
deepak-raj-1 <deepak.kadavergu@gmail.com>
2026-03-14 21:05:57 +0530

    day 23 is the turtle crossing game done

--- day23/scoreboard.py
+++ day23/scoreboard.py
@@ -16,5 +16,5 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
     def game_over(self):
         self.goto(0,0)
-        self.write(f"Game Over!", align="center", font=FONT)
+        self.write(f"Game Over", align="center", font=FONT)
 
